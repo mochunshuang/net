c327ff494024f39e382a01484b5f997faa38a860
mochunshuang <1651798639@qq.com>
2025-04-12 03:26:06 +0800

    IPv4address 测试代码

--- include/detail/ABNF/URI_ABNF.hpp
+++ include/detail/ABNF/URI_ABNF.hpp
@@ -6,6 +6,8 @@
 
 #include "./URI/__IPv6address.hpp"
 
+#include "./URI/__IPv4address.hpp"
+
 /**
  * @brief [ABNF-URI]
  * https://www.rfc-editor.org/rfc/rfc3986.html#appendix-A:~:text=Appendix%20A.%20%20Collected%20ABNF%20for%20URI
