3f480679b05bb6609240fd538ad5ed06ff8c300f
mochunshuang <1651798639@qq.com>
2025-04-17 23:10:39 +0800

    transfer_coding

--- include/__detail/abnf/http/__transfer_parameter.hpp
+++ include/__detail/abnf/http/__transfer_parameter.hpp
@@ -1,12 +1,59 @@
 #pragma once
 
-#include "./__ows.hpp"
+#include "./__token.hpp"
+#include "./__tchar.hpp"
+#include "./__quoted_string.hpp"
 
 namespace mcs::abnf::http
 {
+    // token = 1*tchar
     // transfer-parameter = token BWS "=" BWS ( token / quoted-string )
     constexpr abnf_result auto transfer_parameter(span_param_in sp) noexcept
     {
-        return OWS(sp);
+        const auto k_size = sp.size();
+        if (k_size == 0)
+            return simple_result::fail(0);
+        std::size_t index = 0;
+        while (index < k_size) // token
+        {
+            if (tchar(sp[index]))
+            {
+                ++index;
+                continue;
+            }
+            break;
+        }
+        if (index == 0)
+            return simple_result::fail(index);
+
+        static_assert(not tchar(SP) && not tchar(HTAB) && not tchar('='));
+        while (index < k_size) // BWS
+        {
+            if (sp[index] == SP || sp[index] == HTAB)
+            {
+                ++index;
+                continue;
+            }
+            break;
+        }
+
+        static_assert('=' != SP && '=' != HTAB);
+        if (sp[index] != '=') //  '='
+            return simple_result::fail(index);
+
+        // check: BWS ( token / quoted-string )
+        index += 1;
+        while (index < k_size) // BWS
+        {
+            if (sp[index] == SP || sp[index] == HTAB)
+            {
+                ++index;
+                continue;
+            }
+            break;
+        }
+        const auto k_sub = sp.subspan(index);
+        return (token(k_sub) || quoted_string(k_sub)) ? simple_result::success()
+                                                      : simple_result::fail(index);
     }
 }; // namespace mcs::abnf::http
