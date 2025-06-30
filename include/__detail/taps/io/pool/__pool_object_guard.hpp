49bb63f2bfa8cf7849eae6bcf63b2950ece27fb0
mochunshuang <1651798639@qq.com>
2025-07-01 01:34:18 +0800

    简化 pool_object_guard

--- include/__detail/taps/io/pool/__pool_object_guard.hpp
+++ include/__detail/taps/io/pool/__pool_object_guard.hpp
@@ -12,18 +12,21 @@ namespace mcs::net::io::pool
         pool_object_guard &operator=(const pool_object_guard &) = delete;
         pool_object_guard &operator=(pool_object_guard &&) = delete;
 
-        explicit pool_object_guard(Pool &pool) : buffer_{pool.allocate()}, pool_{pool} {}
-        ~pool_object_guard()
+        constexpr explicit pool_object_guard(Pool &pool)
+            : buffer_{pool.allocate()}, pool_{pool}
         {
-            pool_.deallocate(static_cast<object_type *>(buffer_));
         }
-        object_type *data() const noexcept
+        constexpr ~pool_object_guard() noexcept
         {
-            return static_cast<object_type *>(buffer_);
+            pool_.deallocate(buffer_);
+        }
+        constexpr object_type *data() const noexcept
+        {
+            return buffer_;
         }
 
       private:
-        void *buffer_;
+        object_type *buffer_;
         Pool &pool_;
     };
 }; // namespace mcs::net::io::pool
